beca4bfed311747927cba834d3711348677d7c8a
Rami Elsayed <ramielsayed123@outlook.com>
2022-08-21 20:35:15 +0100

    config and packages

--- src/models/department.js
+++ src/models/department.js
@@ -1,9 +1,14 @@
 class Department {
   constructor(id, name) {
+    const regex = RegExp(/^[\s+]/gm);
+    const invalidName = regex.test(name);
+    if (name === '' || invalidName) {
+      throw new Error("Please enter a dvalid name")
+    }
     this.id = id;
     this.name = name;
   }
-  
+
 }
 
 module.exports = { Department };
---
+++ src/test/department.test.js
@@ -0,0 +1,10 @@
+const {describe, expect, test, it} = require('@jest/globals');
+const { Department } = require("../models/department");
+jest.mock("Department")
+
+describe('Department',() => {
+   
+it("This should retrun the value passed to it"),() => {
+
+}
+})
